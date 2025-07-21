ab5cb3c5e2817338b8821fc0b305d6d709aaaee5
yavidor <yonatan@avidor.org>
2025-07-21 23:55:51 +0300

    Mush

--- run.bash
+++ run.bash
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 docker rm -f bitburner-filesync
 docker build -t bitburner .
-docker run --rm -d -v "${PWD}/src/:/app/src/" -v "${PWD}/NetscriptDefinitions.d.ts:/app/NetscriptDefinitions.d.ts" -p 12525:12525 --name bitburner-filesync bitburner
+docker run --rm -d -v "${PWD}/src/:/app/src/" -v "${PWD}/NetscriptDefinitions.d.ts:/app/NetscriptDefinitions.d.ts" -p 12525:12525 --name bitburner-server bitburner
